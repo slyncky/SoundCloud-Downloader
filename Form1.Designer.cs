34dd621af9dc935f48c67ac22cd3588b220ae360
slyncky <nwscholtz@gmail.com>
2016-09-08 12:58:32 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace SoundCloud_Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnGO = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtDownloadFolder = new System.Windows.Forms.TextBox();
+            this.btnDownloadDirectory = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(14, 95);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(566, 22);
+            this.txtURL.TabIndex = 0;
+            this.txtURL.Click += new System.EventHandler(this.txtURL_Click);
+            this.txtURL.Enter += new System.EventHandler(this.txtURL_Enter);
+            // 
+            // btnGO
+            // 
+            this.btnGO.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnGO.Location = new System.Drawing.Point(286, 145);
+            this.btnGO.Name = "btnGO";
+            this.btnGO.Size = new System.Drawing.Size(149, 41);
+            this.btnGO.TabIndex = 1;
+            this.btnGO.Text = "Download";
+            this.btnGO.UseVisualStyleBackColor = true;
+            this.btnGO.Click += new System.EventHandler(this.btnGO_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(15, 272);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(87, 21);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(338, 188);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 12);
+            this.lblStatus.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(588, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(136, 138);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(64, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 18);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "SoundCloud Downloader";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(645, 281);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 12);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Snowflake ™";
+            // 
+            // txtDownloadFolder
+            // 
+            this.txtDownloadFolder.Enabled = false;
+            this.txtDownloadFolder.Location = new System.Drawing.Point(14, 67);
+            this.txtDownloadFolder.Name = "txtDownloadFolder";
+            this.txtDownloadFolder.Size = new System.Drawing.Size(413, 22);
+            this.txtDownloadFolder.TabIndex = 13;
+            // 
+            // btnDownloadDirectory
+            // 
+            this.btnDownloadDirectory.Location = new System.Drawing.Point(432, 66);
+            this.btnDownloadDirectory.Name = "btnDownloadDirectory";
+            this.btnDownloadDirectory.Size = new System.Drawing.Size(88, 23);
+            this.btnDownloadDirectory.TabIndex = 14;
+            this.btnDownloadDirectory.Text = "DL Directory";
+            this.btnDownloadDirectory.UseVisualStyleBackColor = true;
+            this.btnDownloadDirectory.Click += new System.EventHandler(this.btnDownloadDirectory_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(17, 210);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(707, 23);
+            this.progressBar1.TabIndex = 15;
+            this.progressBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(738, 302);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnDownloadDirectory);
+            this.Controls.Add(this.txtDownloadFolder);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnGO);
+            this.Controls.Add(this.txtURL);
+            this.Font = new System.Drawing.Font("Algerian", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "SoundCloud Downloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnGO;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtDownloadFolder;
+        private System.Windows.Forms.Button btnDownloadDirectory;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+
+namespace SoundCloud_Downloader
+{
+    public partial class Form1 : Form
+    {
+        string songName;
+        string clientId = SoundCloud_Downloader.Properties.Settings.Default.ClientID;
+        public Form1()
+        {
+            InitializeComponent();
+            btnGO.Left = (this.ClientSize.Width - btnGO.Width) / 2;
+            string line;
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            StreamReader file = new StreamReader(path+@"Settings.txt");
+
+            int lenghtOfArray = 0;
+            while ((line = file.ReadLine()) != null)
+            {
+                txtDownloadFolder.Text = line.Substring(line.LastIndexOf("=")+1);
+                lenghtOfArray++;
+            }
+            file.Close();
+        }
+
+        private void btnGO_Click(object sender, EventArgs e)
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.DoWork += new DoWorkEventHandler(doDownload);
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+            worker.RunWorkerAsync();
+        }
+        private void doDownload(object sender, DoWorkEventArgs e)
+        {
+            lblStatus.Invoke(new MethodInvoker(delegate { lblStatus.Text = ""; }));
+            if (!Directory.Exists(txtDownloadFolder.Text))
+            {
+                //lblStatus.Text = "Please correct the download directory";
+                lblStatus.Invoke(new MethodInvoker(delegate { lblStatus.Text = "Please correct the download directory"; }));
+                lblStatus.ForeColor = Color.Red;
+                setLablePosition();
+            }
+            else if (txtURL.Text == "")
+            {
+                //lblStatus.Text = "Please enter a valid URL";
+                lblStatus.Invoke(new MethodInvoker(delegate { lblStatus.Text = "Please enter a valid URL"; }));
+                lblStatus.ForeColor = Color.Red;
+                setLablePosition();
+            }
+            else
+            {
+                try
+                {
+
+                    songName = getStreamURL();
+                    //webBrowser1.Navigate(new Uri(streamURL + "?client_id=2a9ccd31d93cec53b96e4ab9ee8f9db1"));
+                    //txtURL.Clear();
+
+
+                }
+                catch (Exception ex)
+                {
+                    //lblStatus.Text = ex.Message;
+                    lblStatus.Invoke(new MethodInvoker(delegate { lblStatus.Text = ex.Message; }));
+                    lblStatus.ForeColor = Color.Red;
+                    setLablePosition();
+                }
+
+            }
+        }
+      
+        public void setLablePosition()
+        {
+            //lblStatus.Left = (this.ClientSize.Width - lblStatus.Width) / 2;
+            lblStatus.Invoke(new MethodInvoker(delegate { lblStatus.Left = (this.ClientSize.Width - lblStatus.Width) / 2; }));
+        }
+ 
+        public string getStreamURL()
+        {
+                //get the JSON return form the URL
+                WebClient wc = new WebClient();
+                var x = wc.DownloadString(@"https://api.soundcloud.com/resolve?url=" + txtURL.Text.ToString() + @"&_status_format=json&client_id=" + clientId);
+
+                //deserialize the JSON return
+                var jobject = JsonConvert.DeserializeObject<RootObject>(x);
+
+                string streamURL = jobject.stream_url;
+                streamURL = streamURL + "?client_id=" + clientId;
+                //Clipboard.SetText(jobject.title);
+
+
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(streamURL);
+                req.AllowAutoRedirect = false;
+                req.Timeout = 10000;
+                req.Method = "HEAD";
+
+                HttpWebResponse webResponse;
+                using (webResponse = (HttpWebResponse)req.GetResponse())
+                {
+                    if ((int)webResponse.StatusCode >= 300 && (int)webResponse.StatusCode <= 399)
+                    {
+                        string uriString = webResponse.Headers["Location"];
+                        webResponse.Close();
+                        WebClient client = new WebClient();
+                        client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+                         client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+                
+
+                         client.DownloadFileAsync (new Uri (uriString), txtDownloadFolder.Text+@"\"+jobject.title+".mp3");
+
+                         //progressBar1.Visible = true;
+                         progressBar1.Invoke(new MethodInvoker(delegate { progressBar1.Visible = true; }));
+                         //lblStatus.Text = "Downloading...";
+                         lblStatus.Invoke(new MethodInvoker(delegate { lblStatus.Text = "Downloading..."; }));
+                         lblStatus.ForeColor = Color.Red;
+                         setLablePosition();
+                         //btnGO.Enabled = false;
+                         btnGO.Invoke(new MethodInvoker(delegate { btnGO.Enabled = false; }));
+                         
+                    }
+                }
+
+
+
+                return jobject.title;
+        }
+
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            double bytesIn = double.Parse(e.BytesReceived.ToString());
+            double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+            double percentage = bytesIn / totalBytes * 100;
+
+            //progressBar1.Value = int.Parse(Math.Truncate(percentage).ToString());
+            progressBar1.Invoke(new MethodInvoker(delegate { progressBar1.Value = int.Parse(Math.Truncate(percentage).ToString()); }));
+        }
+
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            lblStatus.Invoke(new MethodInvoker(delegate { lblStatus.Text = "Download Completed = " + songName; }));
+            //lblStatus.Text = "Download Completed = " + songName;
+            lblStatus.ForeColor = Color.Green;
+            setLablePosition();
+            //progressBar1.Visible = false;
+            progressBar1.Invoke(new MethodInvoker(delegate { progressBar1.Visible = false; }));
+            //btnGO.Enabled = true;
+            btnGO.Invoke(new MethodInvoker(delegate { btnGO.Enabled = true; }));
+        }
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            //webBrowser1.ShowSaveAsDialog();
+        }
+
+        private void webBrowser1_FileDownload(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtURL.Text = "https://soundcloud.com/wittytunes/samuel-dan-gimme-original-mix";
+            txtURL.ForeColor = Color.Gray;
+            btnGO.Focus();
+        }
+
+        private void txtURL_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtURL_Click(object sender, EventArgs e)
+        {
+            txtURL.Text = "";
+            txtURL.ForeColor = Color.Black;
+        }
+
+        private void btnDownloadDirectory_Click(object sender, EventArgs e)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtDownloadFolder.Text = folderBrowserDialog1.SelectedPath;
+                StreamWriter writer = new StreamWriter(path + "Settings.txt");
+                writer.WriteLine("DLDirectory="+txtDownloadFolder.Text);
+                writer.Close();
+            }
+        }
+
+    }
+
+    public class User
+    {
+        public int id { get; set; }
+        public string kind { get; set; }
+        public string permalink { get; set; }
+        public string username { get; set; }
+        public string last_modified { get; set; }
+        public string uri { get; set; }
+        public string permalink_url { get; set; }
+        public string avatar_url { get; set; }
+    }
+
+    public class RootObject
+    {
+        public string kind { get; set; }
+        public int id { get; set; }
+        public string created_at { get; set; }
+        public int user_id { get; set; }
+        public int duration { get; set; }
+        public bool commentable { get; set; }
+        public string state { get; set; }
+        public int original_content_size { get; set; }
+        public string last_modified { get; set; }
+        public string sharing { get; set; }
+        public string tag_list { get; set; }
+        public string permalink { get; set; }
+        public bool streamable { get; set; }
+        public string embeddable_by { get; set; }
+        public bool downloadable { get; set; }
+        public string purchase_url { get; set; }
+        public object label_id { get; set; }
+        public string purchase_title { get; set; }
+        public string genre { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string label_name { get; set; }
+        public string release { get; set; }
+        public string track_type { get; set; }
+        public string key_signature { get; set; }
+        public string isrc { get; set; }
+        public object video_url { get; set; }
+        public object bpm { get; set; }
+        public object release_year { get; set; }
+        public object release_month { get; set; }
+        public object release_day { get; set; }
+        public string original_format { get; set; }
+        public string license { get; set; }
+        public string uri { get; set; }
+        public User user { get; set; }
+        public string permalink_url { get; set; }
+        public string artwork_url { get; set; }
+        public string waveform_url { get; set; }
+        public string stream_url { get; set; }
+        public int playback_count { get; set; }
+        public int download_count { get; set; }
+        public int favoritings_count { get; set; }
+        public int comment_count { get; set; }
+        public string attachments_uri { get; set; }
+        public string policy { get; set; }
+    }
+}
